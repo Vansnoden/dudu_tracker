4f40bf0075e024df13179492cf4b1e6736c48602
vansnoden <atagong5@gmail.com>
2023-07-24 03:31:21 +0300

    ui setup

--- web/urls.py
+++ web/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('home', home, name="home"),
+    path('', home, name="home"),
 ]
--- web/views.py
+++ web/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+MONTHS = [
+    "Jan","Feb","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"
+]
+
+TSTEPS = [
+    "Monthly", "Yearly"
+]
+
 def home(request):
-    return render(request, "web/index.html")
+    return render(request, "web/index.html", {
+        "months": MONTHS,
+        "tsteps": TSTEPS
+    })
