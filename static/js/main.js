b760196fa0ce373be3a007307b478d9de0eca8dd
vansnoden <atagong5@gmail.com>
2023-07-25 13:17:43 +0300

    ui updated

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,41 @@
 $(document).ready(function(){
-    console.log("script loaded ...");
+
+    // to be hidden
+    $("#constrains").hide();
+    $("#period").hide();
+    $("#calibration").hide();
+
+    // constraint management
+    $("#constraint-show").click((ev) => {
+        $("#constrains").show(400);
+    })
+
+    $("#constraint-dispose").click((ev) => {
+        $("#constrains").hide(400);
+    })
+
+    $("#calibration-show").click((ev) => {
+        $("#calibration").show(400);
+    })
+
+    $("#calibration-hide").click((ev) => {
+        $("#calibration").hide(400);
+    })
+
+    $("#period-hide").click((ev) => {
+        $("#period").hide(400);
+    })
+
+    $("#period-show").click((ev) => {
+        $("#period").show(400);
+    })
+
+    var map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+
+    console.log("script loaded...");
 })
