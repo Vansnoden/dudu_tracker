27c598afa218ba5066e856b067315d9a51de9be5
vansnoden <atagong5@gmail.com>
2023-11-08 13:09:02 +0300

    django application renamed from django_kmap to dudu_tracker

--- dudu_tracker/wsgi.py
+++ dudu_tracker/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django_kmap project.
+WSGI config for dudu_tracker project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_kmap.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dudu_tracker.settings')
 
 application = get_wsgi_application()
