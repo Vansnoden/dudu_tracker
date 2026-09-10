df360c08fc3a1e8798741e8fd5f81d58dff5030f
salvador <atagong5@gmail.com>
2026-09-10 13:44:46 +0300

    update

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,19 +1,30 @@
-from django import forms 
-from django.contrib.auth.models import User 
-from .models import *
-from django.core import validators
-from django.forms import CharField
-import re
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+from .models import UserProfile
 
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
-    def __init__(self, *args, **kwargs):
-        super(UserForm, self).__init__(*args, **kwargs)
-        self.fields['email'].required = True
+    password = forms.CharField(
+        widget=forms.PasswordInput(),
+        validators=[validate_password],  # Enforces Django settings password rules
+        help_text="Enter a strong password."
+    )
+
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].required = True
+
+    def clean_email(self):
+        """Ensure email is unique across accounts."""
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("A user with this email address already exists.")
+        return email
 
 
 class UserProfileForm(forms.ModelForm):
@@ -27,19 +38,10 @@ class LoginForm(forms.Form):
     password = forms.CharField(label="Password", widget=forms.PasswordInput, required=True)
 
 
-
 class PasswordResetForm(forms.Form):
     email = forms.EmailField(label="Email", required=True)
-    new_password = forms.CharField(label="New password", widget=forms.PasswordInput)
-
-    def clean_new_password(self):
-        pattern = re.compile("^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$")
-        password = str(self.cleaned_data['new_password'])
-        if pattern.match(password):
-            return password
-        else:
-            raise forms.ValidationError("Password should contain minimum eight \
-                                        characters, at least one uppercase letter, \
-                                        one lowercase letter, one number and one \
-                                        special character", code="password_error")
-
+    new_password = forms.CharField(
+        label="New password",
+        widget=forms.PasswordInput,
+        validators=[validate_password]
+    )
